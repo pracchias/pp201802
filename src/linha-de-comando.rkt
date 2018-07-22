edb0afdb0b6994477f756a66fd1b98bd4329cbfa
Andre <Andre>
2018-07-21 21:30:44 -0300

    Prototipo de como funcionará a linha de comando. Falhas conhecidas: não reage bem a comandos inesperados.

---
+++ src/linha-de-comando.rkt
@@ -0,0 +1,50 @@
+#lang racket/base
+(require racket/format)
+ 
+
+(define-namespace-anchor a) 
+(define ns (namespace-anchor->namespace a))
+
+
+; ESPERA
+(define (es-init-game) ((println "Iniciando jogo.") (loop "vo-")))
+(define (es-minha-vez) ((println "O jogo não comecou.") (loop "es-")))
+(define (es-passo) ((println "O jogo não começou.") (entrada "es-")))
+(define (es-sair) (exit))
+
+
+; SUA VEZ
+(define (sv-init-game) ((println "Já está rodando. oláaaa") (loop "sv-")))
+(define (sv-minha-vez) ((println "Vai logo!") (loop  "sv-")))
+(define (sv-passo) ((println "Demorou.") (loop  "vo-" )))
+(define (sv-sair) ((println "Essa partida terminou.") (loop "es-"))
+
+; VEZ DO OUTRO
+(define (vo-init-game) ((println "O jogo ja começou.") (loop "vo-")))
+(define (vo-minha-vez) ((println "Mudando para sua vez.") (loop "sv-")))
+(define (vo-passo) ((println "Você não pode passar pelo outro..") (loop "vo-")))
+(define (sv-sair) ((println "Essa partida terminou.") (loop "es-"))
+
+; VEZ DO OUTRO
+(define (init-game) (println "...init-game...." ))
+(define (minha-vez) (println "...minha-vez...." ))
+(define (passo) (println "...passo...."))
+(define (sair) (exit))
+
+
+
+
+
+; Main loop:
+; verifica se há resposta do servidor
+; 
+
+
+(define (le-inputs pref) (read (open-input-string (string-append "(" pref (~a (read)) ")"))))
+
+(define (entrada pref) (eval (le-inputs pref) ns)) ; produces (1 . 2)
+(define (loop pref) ((entrada pref) (loop pref)))
+
+
+(display "Bem vindo. Digite init-game, minha-vez, passo ou sair.\n")
+(entrada "es-")
