01d2e45bfc479e48e91f51eab5cf6d4cdd5e08d3
Andre Barbosa <an.barbosa@outlook.com.br>
2018-07-23 18:15:59 -0300

    Mensagens sendo reconhecidas. Erros sendo identificados. Agora falta uma maneira de interagir com o servidor. O sistema atualmente trava enquanto aguarda o input, como resolver?

--- src/linha-de-comando.rkt
+++ src/linha-de-comando.rkt
@@ -17,13 +17,13 @@
 (define (sv-init-game) ((println "Já está rodando. oláaaa") (loop "sv-")))
 (define (sv-minha-vez) ((println "Vai logo!") (loop  "sv-")))
 (define (sv-passo) ((println "Demorou.") (loop  "vo-" )))
-(define (sv-sair) ((println "Essa partida terminou.") (loop "es-"))
+(define (sv-sair) ((println "Essa partida terminou.") (loop "es-")))
 
 ; VEZ DO OUTRO
 (define (vo-init-game) ((println "O jogo ja começou.") (loop "vo-")))
 (define (vo-minha-vez) ((println "Mudando para sua vez.") (loop "sv-")))
 (define (vo-passo) ((println "Você não pode passar pelo outro..") (loop "vo-")))
-(define (sv-sair) ((println "Essa partida terminou.") (loop "es-"))
+(define (vo-sair) ((println "Essa partida terminou.") (exit)))
 
 ; VEZ DO OUTRO
 (define (init-game) (println "...init-game...." ))
@@ -42,9 +42,19 @@
 
 (define (le-inputs pref) (read (open-input-string (string-append "(" pref (~a (read)) ")"))))
 
-(define (entrada pref) (eval (le-inputs pref) ns)) ; produces (1 . 2)
+(define 
+    (handle-entrada prefixo)
+    (with-handlers ([exn:fail? 
+            (lambda (exn) 
+                ((displayln "Desculpe, comando não reconhecido.")
+                (loop prefixo)))])
+    (eval (le-inputs prefixo) ns)))
+
+(define (entrada pref) (handle-entrada pref))
+    
 (define (loop pref) ((entrada pref) (loop pref)))
 
+(define (main-thread) (lambda () (entrada "es-")))
 
 (display "Bem vindo. Digite init-game, minha-vez, passo ou sair.\n")
 (entrada "es-")
